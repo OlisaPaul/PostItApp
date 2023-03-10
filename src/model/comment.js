927f9b44b1452ffd43ef85a47af21b72adabf4f0
OlisaPaul <odirahchukwumma28@gmail.com>
2023-03-10 10:50:56 +0100

    Feature: added the model for the comments feature

---
+++ src/model/comment.js
@@ -0,0 +1,54 @@
+// Joi is used for data validation
+const Joi = require("joi");
+const mongoose = require("mongoose");
+const validateObjectId = require("../middleware/validateObjectId");
+
+// The schema determines the structure of the comment collection
+const commentSchema = new mongoose.Schema({
+  comment: {
+    type: String,
+    // required makes sure the name is provided by the client.
+    required: true,
+    minlength: 5,
+    maxlength: 500,
+  },
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
+  postId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "post",
+    required: true,
+  },
+  isDeleted: Boolean,
+});
+
+const Comment = mongoose.model("comment", commentSchema);
+
+//To validate the data before sending to the database
+function validate(comment) {
+  const schema = Joi.object({
+    comment: Joi.string().min(4).max(500).required(),
+    userId: Joi.objectId().required(),
+    postId: Joi.objectId().required(),
+  });
+
+  return schema.validate(comment);
+}
+
+function validatePatch(comment) {
+  const schema = Joi.object({
+    comment: Joi.string().min(5).max(500),
+    userId: Joi.objectId(),
+    postId: Joi.objectId(),
+  });
+
+  return schema.validate(comment);
+}
+
+exports.validatePatch = validatePatch;
+exports.validate = validate;
+exports.Comment = Comment;
+exports.commentSchema = commentSchema;
