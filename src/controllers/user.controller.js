7c90c0aab308c66c0bbf07ac7dd479a56487252c
OlisaPaul <odirahchukwumma28@gmail.com>
2023-03-10 23:52:21 +0100

    Fix: made sure a user can only delete their account

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -76,7 +76,7 @@ class UserController {
     if (!user) return res.status(404).send(errorMessage(user));
 
     // makes sure the user can only delete their account
-    if (req.user._id !== user._id)
+    if (req.user._id != user._id)
       return res
         .status(401)
         .send(unAuthMessage(MESSAGES.UNAUTHORIZE("delete")));
